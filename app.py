921e276b726d07d7acba19edcc1ce2a3d1e9e08e
Ali Mirzaev <mrzv.uz@gmail.com>
2021-06-27 21:13:43 -0500

    Finished the book store project which saves the data to csv file

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ Enter:
 - 'l' to list all books
 - 'r' to mark a book as read
 - 'd' to delete a book
-- 'q' to quite
+- 'q' to quit
 
 Your choice: """
 
 
 def menu():
+    database.create_book_table()
     user_input = input(USER_CHOICE)
     while user_input != 'q':
         if user_input == 'a':
@@ -23,42 +24,33 @@ def menu():
             prompt_read_book()
         elif user_input == 'd':
             prompt_delete_book()
-        else:
-            print("Unknown command. Please, try again.")
+
         user_input = input(USER_CHOICE)
 
 
-# def prompt_add_book() - ask for book name and author
 def prompt_add_book():
-    name = input("Enter the new book name: ")
-    author = input("Enter the new book author: ")
+    name = input('Enter the new book name: ')
+    author = input('Enter the new book author: ')
 
     database.add_book(name, author)
 
 
-
-# def list_books() - show all the books in our list
 def list_books():
-    books = database.get_all_books()
-    for book in books:
-        read = "YES" if book["read"] else "NO"
-        print(f"{book['name']} by {book['author']}, read: {read}")
+    for book in database.get_all_books():
+        read = 'YES' if book['read'] else 'NO'
+        print(f"{book['name']} by {book['author']} — Read: {read}")
 
 
-
-# def prompt_read_book() - ask for book name and change it to "read" in our list
 def prompt_read_book():
-    name = input("Enter the name of the book you just finished reading: ")
+    name = input('Enter the name of the book you just finished reading: ')
+
     database.mark_book_as_read(name)
 
 
-
-# def prompt_delete_book() - ask for book name and remove book from list
 def prompt_delete_book():
-    name = input("Enter the name of the book you wish to delete: ")
+    name = input('Enter the name of the book you wish to delete: ')
 
     database.delete_book(name)
-
 
 
 menu()
--- utils/database.py
+++ utils/database.py
@@ -1,53 +1,47 @@
-"""
-Concerned with storing and retrieving books from a csv file.
-Format of the csv file:
-
-name,author,read\n
-name,author,read\n
-name,author,read\n
-"""
+books_file = 'books.txt'
 
 
-books_file = "books.txt"
-
-
-
-def add_book(name, author):
-    with open(books_file, "a") as file:
-        file.write(f"{name},{author},0\n")
-
+def create_book_table():
+    with open(books_file, 'w') as file:
+        pass  # just to make sure the file is there
 
 
 def get_all_books():
-    with open(books_file, "r") as file:
-        lines = [line.strip().split(",") for line in file.readlines()]
+    with open(books_file, 'r') as file:
+        lines = [line.strip().split(',') for line in file.readlines()]
 
-        return [
-            {"name": line[0], "author": line[1], "read": line[2]}
-            for line in lines
-        ]
+    return [
+        {'name': line[0], 'author': line[1], 'read': int(line[2])}
+        for line in lines
+    ]
 
 
+def add_book(name, author):
+    with open(books_file, 'a') as file:
+        file.write(f'{name},{author},0\n')
+
+
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        for book in books:
+            file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 
 def mark_book_as_read(name):
     books = get_all_books()
     for book in books:
-        if book["name"] == name:
-            book["read"] = "1"
+        if book['name'] == name:
+            book['read'] = 1
     _save_all_books(books)
 
 
-
-def _save_all_books(books):
-    with open(books_file, "w") as file:
-        for book in books:
-            file.write(f"{book['name']}, {book['author']}, {book['read']}\n")
-
-
-
-def delete_books(name):
+def delete_book(name):
     books = get_all_books()
-    books = [book for book in books if book["name"] != name]
+    books = [book for book in books if book['name'] != name]
     _save_all_books(books)
 
+
+# def delete_book(name):
+#     for book in books:
+#         if book['name'] == name:
+#             books.remove(book)
